f718787b1403f750922e16b9d510ec0044a02683
jaxangir <jaxangirr0700@gmail.com>
2025-03-16 07:18:11 +0500

    d

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -26,7 +26,7 @@ export default function Sidebar() {
             </li>
           </Link>
         ))}
-      </ul> */}
+      </ul> */} 
       <Menu
         className="h-full rounded-xl"
         style={{
